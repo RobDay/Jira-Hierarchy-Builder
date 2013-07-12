c5eaed7bd1943410799df5ac070483b208d38941
Rob Day <Robert.day@dowjones.com>
2013-07-11 23:42:24 -0400

    Almost working UI reorder.  There is a bug with some duplicate children being added- have to fix it

--- background.js
+++ background.js
@@ -4,7 +4,8 @@ chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 
 		if(request.action == "logmessage"){
-			console.log("Received a message from " + sender.tab.id + ": " + request.message);
+			// console.log("Received a message from " + sender.tab.id + ": " + request.message);
+			console.log(request.message);
 		}
   // If we don't return anything, the message channel will close, regardless
   // of whether we called sendResponse.
